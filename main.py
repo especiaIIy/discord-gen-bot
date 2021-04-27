fff61cc5bb8d16240d314beb1f6d9479cd01c33f
especiaIIy <76669361+especiaIIy@users.noreply.github.com>
2021-04-27 00:20:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-# STATUS: INCOMPLETE
+# STATUS = INCOMPLETE
+# will add create tables function later
 
 import discord
 import sqlite3
+import time
 from discord.ext import commands
 
 client = commands.Bot(command_prefix="?")
@@ -10,12 +12,12 @@ connection = sqlite3.connect('accounts.db')
 
 cursor = connection.cursor()
 
-numba = 0
-total = 0
 
 @client.event
 async def on_ready():
-    stream = discord.Streaming(name="Hulu: {}".format(numba), url="https://twitch.tv/wrlddd")
+    cursor.execute("select * from stock")
+    total = len(cursor.fetchall())
+    stream = discord.Streaming(name="In Stock: {}".format(total), url="https://twitch.tv/wrlddd")
     print('------')
     print(client.user.name + "#" + client.user.discriminator)
     print('------')
@@ -24,15 +26,15 @@ async def on_ready():
 @client.command()
 async def gen(ctx):
     cursor.execute("select * from hulu")
-    res = cursor.fetchall()
-    numba = len(res)
-    tt = total + numba
-    embed = discord.Embed(title = "PV-Gen | In Stock: {}".format(tt), description = """
+    zx = len(cursor.fetchall())
+    cursor.execute("select * from nordvpn")
+    ll = len(cursor.fetchall())
+    cursor.execute("select * from stock")
+    res = len(cursor.fetchall())
+    embed = discord.Embed(title = "PV-Gen | In Stock: {}".format(res), description = """
     Hulu - {}
-    Spotify - 
-    Buffalo Wild Wings - 
-    NordVPN - 
-    """.format(numba), color = 0x3eb489)
+    NordVPN - {}
+    """.format(zx, ll), color = 0x3eb489)
     await ctx.send(embed = embed)
 
 @client.command()
@@ -41,25 +43,26 @@ async def args(ctx, arg1, arg2):
 
 @client.command()
 async def restock(ctx):
-    stream = discord.Streaming(name="restocking combo..", url="https://twitch.tv/restocking")
-    await client.change_presence(activity=stream)
+    await client.change_presence(activity=discord.Streaming(name="restocking combo..", url="https://twitch.tv/restocking"))
     await ctx.send("Enter in Combolist")
 
     msg1 = await client.wait_for('message')
     response1 = (msg1.content)
     zz = (msg1.content.split())
-    v = len(zz)
 
-    await ctx.send("What site is this combolist for? [hulu, netflix] `Note: MAKE SURE YOU DO NOT ENTER IN EXTRA ARGUMENTS`")
+    await ctx.send("What site is this combolist for? [hulu, nordvpn] `Note: MAKE SURE YOU DO NOT ENTER IN EXTRA ARGUMENTS`")
     
     msg2 = await client.wait_for('message')
     response2 = (msg2.content.lower())
 
     for x in zz:
         cursor.execute("INSERT INTO {} VALUES ({})".replace("{}", "'{}'").format(response2, x))
+        cursor.execute("INSERT INTO {} VALUES ({})".replace("{}", "'{}'").format('stock', x))
         connection.commit()
-        stream2 = discord.Streaming(name="restocked", url="https://twitch.tv/restocked")
-        await client.change_presence(activity=stream2)
     
+    await ctx.send("Done.")
+    cursor.execute("select * from stock")
+    total = len(cursor.fetchall())
+    await client.change_presence(activity=discord.Streaming(name="In Stock: {}".format(total), url="https://twitch.tv/wrlddd"))
 
 client.run("token")
